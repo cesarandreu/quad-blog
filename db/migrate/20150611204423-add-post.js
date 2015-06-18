40d9538802b644111e0f7070aac3d73e180face3
Cesar Andreu <cesarandreu@gmail.com>
2015-06-18 01:50:15 -0700

    Add migrations

---
+++ db/migrate/20150611204423-add-post.js
@@ -0,0 +1,40 @@
+module.exports = {
+  up: function (queryInterface, DataTypes) {
+    return queryInterface.createTable('Posts', {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      body: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      tags: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false
+      }
+    })
+  },
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable('Posts')
+  }
+}
