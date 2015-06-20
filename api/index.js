84aa448b4dd03dc811c87e7d56f782db7b8c08c2
Cesar Andreu <cesarandreu@gmail.com>
2015-06-20 01:22:01 -0700

    Add API

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const log = require('debug')('quad-blog:api:loader')
+const controllerLoader = require('./controllers')
+const middleware = require('./utils/middleware')
+const compose = require('composition')
+const assert = require('assert')
+
+/**
+ * API loader
+ * @param {object} options
+ * @param {object} options.models
+ * @returns {function} API application
+ */
+module.exports = function apiLoader ({ models }={}) {
+  assert(models, 'api requires models')
+  log('start')
+
+  const api = compose([
+    middleware.addToContext({ models }),
+    ...controllerLoader()
+  ])
+
+  log('end')
+  return api
+}
